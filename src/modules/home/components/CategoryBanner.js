6d838d0ce72db0eb1aa32effec17dbdd42fe7100
Satpal Tanan <tanansatpal@gmail.com>
2019-03-27 19:54:47 +0530

    Minor fixes

--- src/modules/home/components/CategoryBanner.js
+++ src/modules/home/components/CategoryBanner.js
@@ -5,9 +5,10 @@ function CategoryBanner() {
     <div className="container-fluid px-5px">
       <div className="row mx-0">
         <div className="col-md-6 mb-10px px-5px">
-          <div className="card border-0 text-white text-center"><img
-            src="https://d19m59y37dris4.cloudfront.net/sell/1-2-2/img/photo/christopher-campbell-28571-unsplash.jpg"
-            alt="Card image" className="card-img"/>
+          <div className="card border-0 text-white text-center">
+            <img
+              src="https://d19m59y37dris4.cloudfront.net/sell/1-2-2/img/photo/christopher-campbell-28571-unsplash.jpg"
+              alt="Card" className="card-img"/>
             <div className="card-img-overlay d-flex align-items-center">
               <div className="w-100 py-3">
                 <h2 className="display-3 font-weight-bold mb-4">Top picks</h2><a href="category.html"
@@ -20,7 +21,7 @@ function CategoryBanner() {
         <div className="col-md-6 mb-10px px-5px">
           <div className="card border-0 text-white text-center"><img
             src="https://d19m59y37dris4.cloudfront.net/sell/1-2-2/img/photo/marco-xu-496929-unsplash.jpg"
-            alt="Card image" className="card-img"/>
+            alt="Card" className="card-img"/>
             <div className="card-img-overlay d-flex align-items-center">
               <div className="w-100 py-3">
                 <h2 className="display-3 font-weight-bold mb-4">New arrivals</h2><a href="category.html"
@@ -35,7 +36,7 @@ function CategoryBanner() {
         <div className="col-lg-4 mb-10px px-5px">
           <div className="card border-0 text-center text-white"><img
             src="https://d19m59y37dris4.cloudfront.net/sell/1-2-2/img/photo/benjamin-voros-260869-unsplash.jpg"
-            alt="Card image" className="card-img"/>
+            alt="Card" className="card-img"/>
             <div className="card-img-overlay d-flex align-items-center">
               <div className="w-100">
                 <h2 className="display-4 mb-4">Jackets</h2><a href="category.html"
@@ -48,7 +49,7 @@ function CategoryBanner() {
         <div className="col-lg-4 mb-10px px-5px">
           <div className="card border-0 text-center text-white"><img
             src="https://d19m59y37dris4.cloudfront.net/sell/1-2-2/img/photo/malvestida-magazine-458585-unsplash.jpg"
-            alt="Card image" className="card-img"/>
+            alt="Card" className="card-img"/>
             <div className="card-img-overlay d-flex align-items-center">
               <div className="w-100">
                 <h2 className="display-4 mb-4">Lookbook</h2><a href="category.html"
@@ -61,7 +62,7 @@ function CategoryBanner() {
         <div className="col-lg-4 mb-10px px-5px">
           <div className="card border-0 text-center text-dark"><img
             src="https://d19m59y37dris4.cloudfront.net/sell/1-2-2/img/photo/michael-frattaroli-221247-unsplash.jpg"
-            alt="Card image" className="card-img"/>
+            alt="Card" className="card-img"/>
             <div className="card-img-overlay d-flex align-items-center">
               <div className="w-100">
                 <h2 className="display-4 mb-4">Try this</h2><a href="category.html"
