5f824dccd35181907d06a6eb9d5efe81b751cd84
Satpal Tanan <tanansatpal@gmail.com>
2019-03-27 12:44:57 +0530

    Slides added (Tiny Slider)

--- src/components/home/Slides.js
+++ src/components/home/Slides.js
@@ -1,110 +1,43 @@
 import React from 'react'
-import TinySlider from "tiny-slider-react"
+import {tns} from "tiny-slider/src/tiny-slider.module"
+import "tiny-slider/dist/tiny-slider.css"
+
 
 const settings = {
-  items: 1,
-  lazyload: true,
   nav: false,
   mouseDrag: true,
+  container: '.home-full-slider-wrapper',
+  items: 1,
+  slideBy: 'page',
+  autoplay: true
 };
 
-function Slides() {
+class Slides extends React.Component {
+  slider;
 
-  const imgs = [
-    {
-      "_id": "5bffacdf0baf3b019ace1836",
-      "_metadata": {
-        "i18n": {
-          "EN": {}
-        },
-        "image": {
-          "name": "matheus-ferrero-334418-unsplash.jpg",
-          "type": "image/jpeg",
-          "size": 244213,
-          "versioning": true,
-          "_id": "5bffacda0baf3b019ace17f0"
-        }
-      },
-      "publish": "1",
-      "tags": [],
-      "substore": [],
-      "image": "ms.slides/5c4aac3d2b93e81b85a3ab5b.jpg",
-      "description": "<div class=\"container-fluid h-100 py-5\">\n<div class=\"row align-items-center h-100\">\n<div class=\"col-lg-8 col-xl-6 mx-auto text-white text-center\">\n<h5 class=\"text-uppercase text-white font-weight-light mb-4 letter-spacing-5\">Just arrived</h5>\n<h1 class=\"mb-5 display-2 font-weight-bold text-serif\">Denim Jackets</h1>\n<p class=\"lead mb-4\">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>\n<p><a href=\"category.html\" class=\"btn btn-light\">View collection</a></p>\n</div>\n</div>\n</div>",
-      "sort_order": 0,
-      "created_on": "2018-11-29T09:09:52.801Z",
-      "_created_by": "5b39d8722282ab532a326bc7",
-      "title": "Denim Jackets",
-      "updated_on": "2018-11-29T09:15:33.968Z",
-      "_updated_by": "5b39d8722282ab532a326bc7"
-    },
-    {
-      "_id": "5bffaccbd204a301a395b81e",
-      "_metadata": {
-        "i18n": {
-          "EN": {}
-        },
-        "image": {
-          "name": "ian-dooley-347942-unsplash.jpg",
-          "type": "image/jpeg",
-          "size": 296986,
-          "versioning": true,
-          "_id": "5bffacc09f05300251de8e1b"
-        }
-      },
-      "publish": "1",
-      "tags": [],
-      "substore": [],
-      "image": "ms.slides/5c4aad377abc191bb8d43ebe.jpg",
-      "description": "<div class=\"container-fluid h-100\">\n<div class=\"row align-items-center h-100\">\n<div class=\"col-lg-8 col-xl-6 mx-auto text-white text-center overlay-content\">\n<h1 class=\"mb-4 display-2 text-uppercase font-weight-bold\">Skeleton Tees</h1>\n<p class=\"lead mb-5\">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>\n<p><a href=\"category.html\" class=\"btn btn-light\">Start shopping</a></p>\n</div>\n</div>\n</div>",
-      "sort_order": 0,
-      "created_on": "2018-11-29T09:09:32.239Z",
-      "_created_by": "5b39d8722282ab532a326bc7",
-      "subtitle": "Skeleton Tees",
-      "updated_on": "2018-11-29T09:15:13.087Z",
-      "_updated_by": "5b39d8722282ab532a326bc7"
-    },
-    {
-      "_id": "5bffacb49f05300251de8dd7",
-      "_metadata": {
-        "i18n": {
-          "EN": {}
-        },
-        "image": {
-          "name": "haley-phelps-62815-unsplash.jpg",
-          "type": "image/jpeg",
-          "size": 221114,
-          "versioning": true,
-          "_id": "5bffac91d204a301a395b62d"
-        }
-      },
-      "publish": "1",
-      "tags": [],
-      "substore": [],
-      "description": "<div class=\"container-fluid h-100\">\n<div class=\"row align-items-center h-100\">\n<div class=\"col-lg-8 col-xl-6 mx-auto text-white text-center\">\n<h5 class=\"text-uppercase font-weight-light mb-4 letter-spacing-5\">Our bestseller</h5>\n<h1 class=\"mb-5 display-1 font-weight-bold text-serif\">Skinny Jeans</h1>\n<p><a href=\"category.html\" class=\"btn btn-light\">View collection</a></p>\n</div>\n</div>\n</div>",
-      "image": "ms.slides/5c4aacc22b93e81b85a3aebe.jpg",
-      "sort_order": 0,
-      "created_on": "2018-11-29T09:09:09.720Z",
-      "_created_by": "5b39d8722282ab532a326bc7",
-      "title": "Skinny Jeans",
-      "updated_on": "2018-11-29T09:15:20.909Z",
-      "_updated_by": "5b39d8722282ab532a326bc7"
-    }
-  ];
-  return (<section className="home-full-slider-wrapper mb-10px">
-    <TinySlider settings={settings}>
 
+  componentDidMount() {
+    this.slider = tns(settings);
+  }
+
+  componentWillUnmount() {
+    if (this.slider) this.slider.destroy();
+  }
+
+  render() {
+    const imgs = this.props.slides;
+    return (<section className="home-full-slider-wrapper mb-10px">
       {imgs.map((slide, index) => (
-        <div key={index} className="item home-full-item">
-          <img src={'https://cdn.storehippo.com/s/576253cbd08b24115d6731e6/' + slide.image}
-               alt={slide.title}
-               style={{
-                 width: '100vw',
-                 height: '100vh'
-               }}/>
+        <div>
+          <div key={index} className="item home-full-item">
+            <img src={'https://cdn.storehippo.com/s/576253cbd08b24115d6731e6/' + slide.image}
+                 alt={slide.title}/>
+            {/*{slide.description}*/}
+          </div>
         </div>
       ))}
-    </TinySlider>
-  </section>)
+    </section>)
+  }
 }
 
 export default Slides;
--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -1,9 +1,89 @@
 import React from 'react'
 import Slides from "../home/Slides";
 
+const imgs = [
+  {
+    "_id": "5bffacdf0baf3b019ace1836",
+    "_metadata": {
+      "i18n": {
+        "EN": {}
+      },
+      "image": {
+        "name": "matheus-ferrero-334418-unsplash.jpg",
+        "type": "image/jpeg",
+        "size": 244213,
+        "versioning": true,
+        "_id": "5bffacda0baf3b019ace17f0"
+      }
+    },
+    "publish": "1",
+    "tags": [],
+    "substore": [],
+    "image": "ms.slides/5c4aac3d2b93e81b85a3ab5b.jpg",
+    "description": "<div class=\"container-fluid h-100 py-5\">\n<div class=\"row align-items-center h-100\">\n<div class=\"col-lg-8 col-xl-6 mx-auto text-white text-center\">\n<h5 class=\"text-uppercase text-white font-weight-light mb-4 letter-spacing-5\">Just arrived</h5>\n<h1 class=\"mb-5 display-2 font-weight-bold text-serif\">Denim Jackets</h1>\n<p class=\"lead mb-4\">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>\n<p><a href=\"category.html\" class=\"btn btn-light\">View collection</a></p>\n</div>\n</div>\n</div>",
+    "sort_order": 0,
+    "created_on": "2018-11-29T09:09:52.801Z",
+    "_created_by": "5b39d8722282ab532a326bc7",
+    "title": "Denim Jackets",
+    "updated_on": "2018-11-29T09:15:33.968Z",
+    "_updated_by": "5b39d8722282ab532a326bc7"
+  },
+  {
+    "_id": "5bffaccbd204a301a395b81e",
+    "_metadata": {
+      "i18n": {
+        "EN": {}
+      },
+      "image": {
+        "name": "ian-dooley-347942-unsplash.jpg",
+        "type": "image/jpeg",
+        "size": 296986,
+        "versioning": true,
+        "_id": "5bffacc09f05300251de8e1b"
+      }
+    },
+    "publish": "1",
+    "tags": [],
+    "substore": [],
+    "image": "ms.slides/5c4aad377abc191bb8d43ebe.jpg",
+    "description": "<div class=\"container-fluid h-100\">\n<div class=\"row align-items-center h-100\">\n<div class=\"col-lg-8 col-xl-6 mx-auto text-white text-center overlay-content\">\n<h1 class=\"mb-4 display-2 text-uppercase font-weight-bold\">Skeleton Tees</h1>\n<p class=\"lead mb-5\">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>\n<p><a href=\"category.html\" class=\"btn btn-light\">Start shopping</a></p>\n</div>\n</div>\n</div>",
+    "sort_order": 0,
+    "created_on": "2018-11-29T09:09:32.239Z",
+    "_created_by": "5b39d8722282ab532a326bc7",
+    "subtitle": "Skeleton Tees",
+    "updated_on": "2018-11-29T09:15:13.087Z",
+    "_updated_by": "5b39d8722282ab532a326bc7"
+  },
+  {
+    "_id": "5bffacb49f05300251de8dd7",
+    "_metadata": {
+      "i18n": {
+        "EN": {}
+      },
+      "image": {
+        "name": "haley-phelps-62815-unsplash.jpg",
+        "type": "image/jpeg",
+        "size": 221114,
+        "versioning": true,
+        "_id": "5bffac91d204a301a395b62d"
+      }
+    },
+    "publish": "1",
+    "tags": [],
+    "substore": [],
+    "description": "<div class=\"container-fluid h-100\">\n<div class=\"row align-items-center h-100\">\n<div class=\"col-lg-8 col-xl-6 mx-auto text-white text-center\">\n<h5 class=\"text-uppercase font-weight-light mb-4 letter-spacing-5\">Our bestseller</h5>\n<h1 class=\"mb-5 display-1 font-weight-bold text-serif\">Skinny Jeans</h1>\n<p><a href=\"category.html\" class=\"btn btn-light\">View collection</a></p>\n</div>\n</div>\n</div>",
+    "image": "ms.slides/5c4aacc22b93e81b85a3aebe.jpg",
+    "sort_order": 0,
+    "created_on": "2018-11-29T09:09:09.720Z",
+    "_created_by": "5b39d8722282ab532a326bc7",
+    "title": "Skinny Jeans",
+    "updated_on": "2018-11-29T09:15:20.909Z",
+    "_updated_by": "5b39d8722282ab532a326bc7"
+  }
+];
 
 function Main() {
-  return (<Slides/>
+  return (<Slides slides={imgs}/>
   )
 }
 
