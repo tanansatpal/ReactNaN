1bbebfca86469dc6e92fb9f1c270baa86f0b044c
Satpal Tanan <tanansatpal@gmail.com>
2019-04-14 23:58:43 +0530

    Icons added

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import routes from "./Routes";
 import {withRouter} from "react-router";
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faBars, faAngleDown, faTimes, faArrowRight} from '@fortawesome/free-solid-svg-icons'
+import SvgSprites from "./components/SvgSprites";
 
+library.add(faBars, faAngleDown, faTimes, faArrowRight);
 const HeaderWithRouter = withRouter(Header);
 
 function App() {
     return (
         <div className="page-container">
+            <SvgSprites/>
             <HeaderWithRouter/>
             <Switch>
                 {routes.map((route, index) => (<Route key={index} {...route} />))}
